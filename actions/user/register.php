d340d3b23e24f9e3a95677a518410e239f54c462
Roman <cikipuki10@gmail.com>
2024-01-30 19:42:48 +0200

    registration form validation

---
+++ actions/user/register.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../../app/requires.php';
+
+$email = $_POST['email'];
+$name = $_POST['name'];
+$dob = $_POST['dob'];
+$password = $_POST['password'];
+$password_reset = $_POST['password_reset'];
+
+$error = false;
+
+$fields = [
+    'email' => [
+        'value' => $email,
+        'error' => false
+    ],
+    'name' => [
+        'value' => $name,
+        'error' => false
+    ],
+    'dob' => [
+        'value' => $dob,
+        'error' => false
+    ],
+    'password' => [
+        'error' => false
+    ],
+    'password_reset' => [
+        'error' => false
+    ],
+];
+
+
+if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $fields['email']['error'] = true;
+    $error = true;
+}
+
+if (empty($name)) {
+    $fields['name']['error'] = true;
+    $error = true;
+}
+
+if (empty($dob)) {
+    $fields['dob']['error'] = true;
+    $error = true;
+}
+
+if ($password !== $password_reset || empty($password) || empty($password_reset)) {
+    $fields['password']['error'] = true;
+    $fields['password_reset']['error'] = true;
+    $error = true;
+}
+
+if ($error) {
+    $_SESSION['fields'] = $fields;
+    header('Location: /register.php');
+}
--- register.php
+++ register.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,22 +16,32 @@
     
     <?php
         require_once __DIR__ .  '/components/header.php';
+        require_once __DIR__ .  '/app/requires.php';
     ?>
 
     <section class="add">
         <div class="container">
             <h1 class="add__title">Регистрация</h1>
-            <form class="add__form">
+            <form class="add__form" action='/actions/user/register.php' method='POST'>
+                <?php
+                    if(isset($_SESSION['fields'])) {
+                ?>
+                    <div class='add__validate'>Проверьте правильность заполнение полей</div>
+                <?php
+                    $fields = $_SESSION['fields'];
+                    unset($_SESSION['fields']);
+                }
+                ?>
                 <label>E-mail</label>
-                <input class="add__input-tema" type="text">
+                <input value="<?= $fields['email']['value'] ?? '' ?>" class="add__input-tema <?= $fields['email']['error'] ? 'add__input-color' : '' ?>" type="text" name='email' require>
                 <label>ФИО</label>
-                <input class="add__input-img" type="text">
+                <input value="<?= $fields['name']['value'] ?? '' ?>" class="add__input-tema <?= $fields['name']['error'] ? 'add__input-color' : '' ?>" type="text" name='name' require>
                 <label>Дата рождения</label>
-                <input class="add__input-img" type="date">
+                <input value="<?= $fields['dob']['value'] ?? '' ?>" class="add__input-tema <?= $fields['dob']['error'] ? 'add__input-color' : '' ?>" type="date" name='dob' require>
                 <label>Пароль</label>
-                <input class="add__input-img" type="text">
+                <input class="add__input-tema <?= $fields['password']['error'] ? 'add__input-color' : '' ?>" type="text" name='password' require>
                 <label>Подтверждение пароля</label>
-                <input class="add__input-img" type="text">
+                <input class="add__input-tema <?= $fields['password_reset']['error'] ? 'add__input-color' : '' ?>" type="text" name='password_reset' require>
                 <input class="add__btn" type="submit" value="Создать Аккаунт">
             </form>
         </div>
