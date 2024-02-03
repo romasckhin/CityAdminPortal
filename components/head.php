5bccbe13e72f6bb45fce5d7315d16a9914b87ac8
Roman <cikipuki10@gmail.com>
2024-02-03 15:49:54 +0200

    accessibility

--- components/head.php
+++ components/head.php
@@ -3,3 +3,7 @@
     <link rel="stylesheet" href="src/scss/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;400;500;600&display=swap" rel="stylesheet">
     <script defer src="src/js/script.js"></script>
+
+<?php
+    require_once __DIR__ . '/../app/requires.php';
+?>
--- register.php
+++ register.php
@@ -16,7 +16,6 @@ session_start();
     
     <?php
         require_once __DIR__ .  '/components/header.php';
-        require_once __DIR__ .  '/app/requires.php';
     ?>
 
     <section class="add">
